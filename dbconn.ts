8a5b99b0e98c08b894314c32d80913db20871e72
pumitle <65011212026@msu.ac.th>
2024-03-04 23:32:51 +0700

    back_end 4/3/67

--- dbconn.ts
+++ dbconn.ts
@@ -4,8 +4,8 @@ export const conn = mysql.createPool(
     {
         connectionLimit: 10,
         host:"sql6.freemysqlhosting.net",
-        user:"sql6686892",
-        password:"Ri3rhjlqu5",
-        database:"sql6686892"
+        user:"sql6688665",
+        password:"cFYiar1Rzt",
+        database:"sql6688665"
     }
 );
