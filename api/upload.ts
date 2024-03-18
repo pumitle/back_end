4de38f7040f0f8df56333ce6286ed66a5b1c8360
pumitle <65011212026@msu.ac.th>
2024-03-18 22:13:47 +0700

    upapi

--- api/upload.ts
+++ api/upload.ts
@@ -113,27 +113,68 @@ router.get("/detailcar/:id", (req, res) => {
   });
 });
 
+// router.get("/rewind/:id", (req, res) => {
+//   const id = req.params.id;
+//   const sql = `
+//     SELECT 
+//     Upload_img.upid, 
+//     Upload_img.*, 
+//     User.*, 
+//     SUM(COALESCE(vote.score, 100)) AS total_score,
+//     DATE(vote.vote_date) AS vote_date
+// FROM 
+//     Upload_img
+//     LEFT JOIN vote ON Upload_img.upid = vote.up_fk_id 
+//     LEFT JOIN User ON Upload_img.uid_user = User.uid 
+// WHERE 
+//     Upload_img.upid = ?
+//     AND DATE(vote.vote_date) <= CURDATE() -- เลือกเฉพาะวันที่น้อยกว่าหรือเท่ากับวันปัจจุบัน
+//     AND DATE(vote.vote_date) >= DATE_SUB(CURDATE(), INTERVAL 7 DAY) -- และมากกว่าหรือเท่ากับ 7 วันก่อนหน้านี้
+// GROUP BY 
+//     Upload_img.upid, DATE(vote.vote_date)
+// ORDER BY
+//     DATE(vote.vote_date) DESC;
+//     `;
+
+//   conn.query(sql, [id], (err, result) => {
+//     if (err) {
+//       res.json(err);
+//     } else {
+//       res.json(result);
+//     }
+//   });
+// });
+
 router.get("/rewind/:id", (req, res) => {
   const id = req.params.id;
   const sql = `
-    SELECT 
-    Upload_img.upid, 
-    Upload_img.*, 
-    User.*, 
-    SUM(COALESCE(vote.score, 100)) AS total_score,
-    DATE(vote.vote_date) AS vote_date
+  SELECT 
+  Upload_img.upid, 
+  Upload_img.*, 
+  User.*, 
+  DATE(vote.vote_date) AS vote_date,
+  CASE 
+      WHEN DATE(vote.vote_date) = (SELECT MIN(DATE(v1.vote_date)) FROM vote v1 WHERE v1.up_fk_id = Upload_img.upid) 
+          THEN SUM(COALESCE(vote.score, 100))
+      ELSE 
+          SUM(COALESCE(vote.score, 100)) + (
+              SELECT SUM(COALESCE(v1.score, 100))
+              FROM vote v1
+              WHERE Upload_img.upid = v1.up_fk_id 
+                  AND DATE(v1.vote_date) < DATE(vote.vote_date)
+          )
+  END AS total_score
 FROM 
-    Upload_img
-    LEFT JOIN vote ON Upload_img.upid = vote.up_fk_id 
-    LEFT JOIN User ON Upload_img.uid_user = User.uid 
+  Upload_img
+  LEFT JOIN vote ON Upload_img.upid = vote.up_fk_id 
+  LEFT JOIN User ON Upload_img.uid_user = User.uid 
 WHERE 
-    Upload_img.upid = ?
-    AND DATE(vote.vote_date) <= CURDATE() -- เลือกเฉพาะวันที่น้อยกว่าหรือเท่ากับวันปัจจุบัน
-    AND DATE(vote.vote_date) >= DATE_SUB(CURDATE(), INTERVAL 7 DAY) -- และมากกว่าหรือเท่ากับ 7 วันก่อนหน้านี้
+  Upload_img.upid = ?
 GROUP BY 
-    Upload_img.upid, DATE(vote.vote_date)
+  Upload_img.upid, DATE(vote.vote_date), vote_date -- รวมคะแนนตามวันที่
 ORDER BY
-    DATE(vote.vote_date) DESC;
+  DATE(vote.vote_date) DESC;
+
     `;
 
   conn.query(sql, [id], (err, result) => {
@@ -144,6 +185,7 @@ ORDER BY
     }
   });
 });
+
 
 router.get("/top10rank", (req, res) => {
   const sql = `
